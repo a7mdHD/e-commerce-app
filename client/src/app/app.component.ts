cb84fd4889e9ef53585073bd211ff71c9fc37de1
A7md <ahmed.hd1011@gmail.com>
2021-03-21 06:25:21 +0200

    End of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +7,12 @@ import { IProduct } from './models/product';
 })
 
 export class AppComponent implements OnInit {
-  title = 'SkiNet';
-  products: IProduct[] = [];
+  title = 'E-Commerce';
 
-  constructor(private http: HttpClient) {
-    this.products = [];
+  constructor() {
    }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/products?pageSize=50').subscribe((response: any) => {
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
   }
 }
+
