5b1261bc9a82891e8a707f82222782a283f42db0
A7md <ahmed.hd1011@gmail.com>
2021-03-28 21:50:06 +0200

    End of section 13

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -24,11 +25,21 @@ namespace API
         {
 
             services.AddControllers();
+
             services.AddDbContext<StoreContext>(x=> 
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            services.AddSingleton<IConnectionMultiplexer>(c => {
+                var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
+
             services.AddAutoMapper(typeof(MappingProfiles));
+
             services.AddApplicationServices();
+
             services.AddSwaggerDocumentation();
+
             services.AddCors(opt =>
             {
                 opt.AddPolicy("CorsPolicy", policy =>
