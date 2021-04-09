6434d5e64c13fd4dc71a6f92d2a0a93a44fc4002
A7md <ahmed.hd1011@gmail.com>
2021-04-09 21:48:49 +0200

    End of section 15

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using Infrastructure.Data;
+using Infrastructure.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -29,6 +30,10 @@ namespace API
             services.AddDbContext<StoreContext>(x=> 
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
+            services.AddDbContext<AppIdentityDbContext>(x => {
+                x.UseSqlite(_config.GetConnectionString("IdentityConnection"));
+            });
+
             services.AddSingleton<IConnectionMultiplexer>(c => {
                 var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
                 return ConnectionMultiplexer.Connect(configuration);
@@ -37,6 +42,8 @@ namespace API
             services.AddAutoMapper(typeof(MappingProfiles));
 
             services.AddApplicationServices();
+
+            services.AddIdentityServices(_config);
 
             services.AddSwaggerDocumentation();
 
@@ -68,6 +75,8 @@ namespace API
             app.UseStaticFiles();
 
             app.UseCors("CorsPolicy");
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
